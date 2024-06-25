400a0435a5ad57b71b82132b2c4b983259723b90
Sizzy <84314549+sizan19@users.noreply.github.com>
2024-06-25 13:39:50 +0545

    send

--- Programmingassignment/UserControl3billing.Designer.cs
+++ Programmingassignment/UserControl3billing.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl3billing));
-            this.label8 = new System.Windows.Forms.Label();
+            this.labelTotalAmount = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.additemSubmit = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
@@ -41,11 +41,11 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textboxItemQuantity = new System.Windows.Forms.NumericUpDown();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxTotal = new System.Windows.Forms.TextBox();
+            this.textboxItemPrice = new System.Windows.Forms.TextBox();
+            this.textboxItemName = new System.Windows.Forms.TextBox();
             this.txtSearchBox = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -56,19 +56,19 @@
             this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textboxItemQuantity)).BeginInit();
             this.SuspendLayout();
             // 
-            // label8
+            // labelTotalAmount
             // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label8.Location = new System.Drawing.Point(980, 403);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(62, 24);
-            this.label8.TabIndex = 55;
-            this.label8.Text = "Rs 00";
+            this.labelTotalAmount.AutoSize = true;
+            this.labelTotalAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotalAmount.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelTotalAmount.Location = new System.Drawing.Point(980, 403);
+            this.labelTotalAmount.Name = "labelTotalAmount";
+            this.labelTotalAmount.Size = new System.Drawing.Size(62, 24);
+            this.labelTotalAmount.TabIndex = 55;
+            this.labelTotalAmount.Text = "Rs 00";
             // 
             // button1
             // 
@@ -106,8 +106,8 @@
             // 
             // dataGridView1
             // 
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Red;
-            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.Red;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle5;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -171,15 +171,16 @@
             this.panel1.Size = new System.Drawing.Size(651, 5);
             this.panel1.TabIndex = 49;
             // 
-            // numericUpDown1
+            // textboxItemQuantity
             // 
-            this.numericUpDown1.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.numericUpDown1.Location = new System.Drawing.Point(388, 159);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(151, 31);
-            this.numericUpDown1.TabIndex = 48;
+            this.textboxItemQuantity.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textboxItemQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxItemQuantity.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textboxItemQuantity.Location = new System.Drawing.Point(388, 159);
+            this.textboxItemQuantity.Name = "textboxItemQuantity";
+            this.textboxItemQuantity.Size = new System.Drawing.Size(151, 31);
+            this.textboxItemQuantity.TabIndex = 48;
+            this.textboxItemQuantity.ValueChanged += new System.EventHandler(this.textboxItemQuantity_ValueChanged);
             // 
             // listBox1
             // 
@@ -192,39 +193,42 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(210, 276);
             this.listBox1.TabIndex = 47;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
-            // textBox4
+            // textBoxTotal
             // 
-            this.textBox4.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox4.Location = new System.Drawing.Point(710, 161);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(210, 24);
-            this.textBox4.TabIndex = 46;
+            this.textBoxTotal.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBoxTotal.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTotal.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBoxTotal.Location = new System.Drawing.Point(710, 161);
+            this.textBoxTotal.Name = "textBoxTotal";
+            this.textBoxTotal.Size = new System.Drawing.Size(210, 24);
+            this.textBoxTotal.TabIndex = 46;
             // 
-            // textBox3
+            // textboxItemPrice
             // 
-            this.textBox3.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox3.Location = new System.Drawing.Point(710, 77);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(210, 24);
-            this.textBox3.TabIndex = 45;
+            this.textboxItemPrice.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textboxItemPrice.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textboxItemPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxItemPrice.ForeColor = System.Drawing.SystemColors.Window;
+            this.textboxItemPrice.Location = new System.Drawing.Point(710, 77);
+            this.textboxItemPrice.Name = "textboxItemPrice";
+            this.textboxItemPrice.ReadOnly = true;
+            this.textboxItemPrice.Size = new System.Drawing.Size(210, 24);
+            this.textboxItemPrice.TabIndex = 45;
             // 
-            // textBox1
+            // textboxItemName
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox1.Location = new System.Drawing.Point(388, 77);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(151, 24);
-            this.textBox1.TabIndex = 44;
+            this.textboxItemName.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textboxItemName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textboxItemName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxItemName.ForeColor = System.Drawing.SystemColors.Window;
+            this.textboxItemName.Location = new System.Drawing.Point(388, 77);
+            this.textboxItemName.Name = "textboxItemName";
+            this.textboxItemName.ReadOnly = true;
+            this.textboxItemName.Size = new System.Drawing.Size(151, 24);
+            this.textboxItemName.TabIndex = 44;
             // 
             // txtSearchBox
             // 
@@ -236,6 +240,7 @@
             this.txtSearchBox.Name = "txtSearchBox";
             this.txtSearchBox.Size = new System.Drawing.Size(210, 24);
             this.txtSearchBox.TabIndex = 43;
+            this.txtSearchBox.TextChanged += new System.EventHandler(this.txtSearchBox_TextChanged);
             // 
             // label6
             // 
@@ -319,18 +324,18 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(34)))), ((int)(((byte)(44)))));
-            this.Controls.Add(this.label8);
+            this.Controls.Add(this.labelTotalAmount);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.additemSubmit);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textboxItemQuantity);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxTotal);
+            this.Controls.Add(this.textboxItemPrice);
+            this.Controls.Add(this.textboxItemName);
             this.Controls.Add(this.txtSearchBox);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -344,7 +349,7 @@
             this.Load += new System.EventHandler(this.UserControl3billing_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textboxItemQuantity)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -352,7 +357,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labelTotalAmount;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button additemSubmit;
         private System.Windows.Forms.DataGridView dataGridView1;
@@ -363,11 +368,11 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown textboxItemQuantity;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxTotal;
+        private System.Windows.Forms.TextBox textboxItemPrice;
+        private System.Windows.Forms.TextBox textboxItemName;
         private System.Windows.Forms.TextBox txtSearchBox;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
--- Programmingassignment/UserControl3billing.cs
+++ Programmingassignment/UserControl3billing.cs
@@ -12,6 +12,8 @@ namespace Programmingassignment
 {
     public partial class UserControl3billing : UserControl
     {
+        function fn = new function();
+        string query;
         public UserControl3billing()
         {
             InitializeComponent();
@@ -19,6 +21,44 @@ namespace Programmingassignment
 
         private void UserControl3billing_Load(object sender, EventArgs e)
         {
+
+        }
+
+        // Search the items //
+        private void txtSearchBox_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            query = "select item_name from items where item_name like'" + txtSearchBox.Text + "%'";
+            DataSet ds = fn.GetData(query);
+
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                listBox1.Items.Add(ds.Tables[0].Rows[i][0].ToString());
+            }
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textboxItemQuantity.ResetText();
+            textBoxTotal.ResetText();
+
+            string text = listBox1.GetItemText(listBox1.SelectedItem);
+            textboxItemName.Text = text;
+            query = "select item_price from items where item_name = '" + text + "'";
+            DataSet ds = fn.GetData(query);
+
+            textboxItemPrice.Text = ds.Tables[0].Rows[0][0].ToString();
+
+
+        }
+
+        private void textboxItemQuantity_ValueChanged(object sender, EventArgs e)
+        {
+            Int64 qty = Int64.Parse(textboxItemQuantity.Value.ToString());
+            Int64 price = Int64.Parse(textboxItemPrice.Text.ToString());
+            textBoxTotal.Text = (qty * price).ToString();
 
         }
     }
